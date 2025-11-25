42ed2cea9eb8fafc526d3308afceab703737405f
Diego Alves de Oliveira <diego@Diegos-MacBook-Air-2.local>
2025-11-25 22:58:44 +0000

    feat(operator-backend): Implementacao da logica de salvamento no banco com injecao de dependencia e identificacao de usuario via GUID

--- VanSpeedLogistics/Controllers/OperatorController.cs
+++ VanSpeedLogistics/Controllers/OperatorController.cs
@@ -1,5 +1,12 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using VanSpeedLogistics.Data;
+using VanSpeedLogistics.Models;
+using VanSpeedLogistics.Models.Entities;
+
 
 namespace VanSpeedLogistics.Controllers;
 
@@ -11,14 +18,58 @@ namespace VanSpeedLogistics.Controllers;
 [Authorize(Roles = "Operator, Manager")]
 public class OperatorController : Controller
 {
+    private readonly ApplicationDbContext _context;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public OperatorController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+    {
+        _context = context;
+        _userManager = userManager;
+    }
+    
     /// <summary>
     /// Action Index (GET): Responsável por carregar o formulário de lançamento de dados.
     /// Retorna a View onde o operador irá preencher seus registros diários.
     /// </summary>
     /// <returns>A View contendo o formulário diário de registro.</returns>
+    [HttpGet]
     public IActionResult Index()
     {
         return View();
+    }
+
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Index(DeliveryViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View("Index");
+        }
+        
+        // Identificando quem é o motorista logado
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        // Transformar ViewModel em um objeto final que vai para o MySQL.
+        var record = new DeliveryRecord
+        {
+            DriverId = userId,
+            Date = DateTime.Now,
+            DeliveriesCount = model.DeliveriesCount,
+            CollectionsCount = model.CollectionsCount,
+            RetournsCount = model.RetournsCount,
+            Notes = model.Notes
+        };
+        // o processo de gravar os dados no banco começar aqui onde estou escrevendo os dados na memória
+        _context.DeliveryRecords.Add(record);
+        
+        // Esse trecho commita a transação no MySQL, gerando o insert into.
+        await _context.SaveChangesAsync();
+        
+        // Redirecionando o usuário par auma página de sucesso.
+        TempData["SuccessMessage"] = "Record added successfully";
+        return RedirectToAction("Index", "Home");
     }
     
 }
